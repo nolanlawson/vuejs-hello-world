928e7db0eaf8d334c00c6c1efa3c2f04020a7222
Nolan Lawson <nolan@nolanlawson.com>
2017-12-26 16:30:45 -0800

    step 3 of ssr

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -59,25 +60,6 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? { safe: true, map: { inline: false } }
         : { safe: true }
     }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: process.env.NODE_ENV === 'testing'
-        ? 'index.html'
-        : config.build.index,
-      template: 'index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
-    }),
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
@@ -119,7 +101,10 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // This plugins generates `vue-ssr-client-manifest.json` in the
+    // output directory.
+    new VueSSRClientPlugin()
   ]
 })
 
--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const path = require('path')
 const { createBundleRenderer } = require('vue-server-renderer')
 const json = require('../dist/vue-ssr-server-bundle.json')
 const template = fs.readFileSync(path.join(__dirname, './index.html'), 'utf8')
+const clientManifest = require('../dist/vue-ssr-client-manifest.json')
 
 const renderer = createBundleRenderer(json, {
   runInNewContext: true,
-  template
+  template,
+  clientManifest
 })
 
 const context = { url: '/' }
@@ -16,7 +18,7 @@ renderer.renderToString(context, (err, html) => {
     console.error(err)
     process.exit(1)
   } else {
-    fs.writeFileSync(path.join(__dirname, '../index.html'), html, 'utf8')
+    fs.writeFileSync(path.join(__dirname, '../dist/index.html'), html, 'utf8')
     process.exit(0)
   }
 })
